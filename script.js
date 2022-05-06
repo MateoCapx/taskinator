f7ad8c72a8156bd4a0143c3c802ad878646c7ac0
Mateo <mateom.realtor@gmail.com>
2022-05-06 15:01:48 -0400

    Updated form element

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
-    let buttonEl = document.querySelector("#save-task"); 
+    let formEl = document.querySelector("#task-form"); 
     let tasksToDoEl = document.querySelector("#tasks-to-do"); 
     
-    let createTaskHandler = function() { 
+    let createTaskHandler = function(event) { 
+        event.preventDefault();
+       
       let listItemEl = document.createElement("li");       //   Create a new task item.
       listItemEl.className = "task-item";                 //   Style the new task item.
       listItemEl.textContent = "This is a new task.";     //   Add the text.
       tasksToDoEl.appendChild(listItemEl);                //   Append this element to the task list.
+
+    
       }; 
     
-    buttonEl.addEventListener("click", createTaskHandler);
+    formEl.addEventListener("click", createTaskHandler);
