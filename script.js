b76ddad72dc7e8b0f6af44185d8f1a2322ec48c1
Mateo <mateom.realtor@gmail.com>
2022-05-07 14:43:44 -0400

    Added form submissions with js

--- script.js
+++ script.js
@@ -3,14 +3,32 @@
     let tasksToDoEl = document.querySelector("#tasks-to-do"); 
     
     let createTaskHandler = function(event) { 
-        event.preventDefault();
+        event.preventDefault();   // Prevents the the normal behavior of an element
        
-      let listItemEl = document.createElement("li");       //   Create a new task item.
-      listItemEl.className = "task-item";                 //   Style the new task item.
-      listItemEl.textContent = "This is a new task.";     //   Add the text.
-      tasksToDoEl.appendChild(listItemEl);                //   Append this element to the task list.
+        let taskNameInput = document.querySelector("input[name='task-name']").value ;  // we're trying to select an HTML element by one of its attributes.
+        let taskTypeInput = document.querySelector("select[name='task-type']").value;
+        console.log(taskTypeInput);
 
-    
+        // create list item
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+
+        // create div to hold task info and add to list item
+        let taskInfoEl = document.createElement("div");
+        
+        // give it a class name
+        taskInfoEl.className = "task-info";
+       
+        // add HTML content to div
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+        listItemEl.appendChild(taskInfoEl);
+
+        // add entire list item to list
+        tasksToDoEl.appendChild(listItemEl);
       }; 
     
     formEl.addEventListener("click", createTaskHandler);
+
+
+    //   Add the text. //  We want to get the task name we just stored in taskNameInput and add it to the listItemEl variable
